4ccc7adacf475e0ae40b66a21a52f4de9f7b31cd
Jamil Abbas <jamil.abbas813@gmail.com>
2019-07-21 18:45:17 +0500

    prepare for deploy

--- server.js
+++ server.js
@@ -35,6 +35,16 @@ app.use("/api/arts", arts);
 //Likes Route
 app.use("/api/likes", likes);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.get("/", (req, res) => res.send("Hello World!"));
 const port = process.env.port || 8000;
 app.listen(port, () => console.log(`app listenting at port ${port} `));
