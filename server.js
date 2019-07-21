de83c7305e8011ca7fef85c2f7a6b0e4b3f26707
Jamil Abbas <jamil.abbas813@gmail.com>
2019-07-21 18:57:48 +0500

    fixed issue

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const profile = require("./routes/api/profile");
 const arts = require("./routes/api/arts");
 const likes = require("./routes/api/likes");
 const passport = require("passport");
+const path = require("path");
 
 const app = express();
 const db = require("./config/keys").mongoURI;
@@ -36,12 +37,12 @@ app.use("/api/arts", arts);
 app.use("/api/likes", likes);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
