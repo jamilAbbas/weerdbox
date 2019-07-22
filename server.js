0b4ccda674e09bca7eda23e20d36cd51f9e49193
Jamil Abbas <jamil.abbas813@gmail.com>
2019-07-22 17:02:29 +0500

    server

--- server.js
+++ server.js
@@ -36,15 +36,14 @@ app.use("/api/arts", arts);
 //Likes Route
 app.use("/api/likes", likes);
 
-// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("./client/build"));
-}
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+  app.use(express.static("client/build"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`app listenting at port ${port} `));
