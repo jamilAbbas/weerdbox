01ddad36db88cb060da02d655325eca2525318cc
Jamil Abbas <jamil.abbas813@gmail.com>
2019-07-23 16:47:56 +0500

    added keys dev

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:
+      "mongodb+srv://weerdboxadmin:Getwb001@cluster0-dtscb.mongodb.net/test?retryWrites=true&w=majority", //Weerdbox
+    secretOrKey: "secret"
+  };
+  
